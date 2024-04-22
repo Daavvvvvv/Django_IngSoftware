6f5746742f6992dd1bfffcbe9853387dfb93d940
Daavvvvvv <davidvelez006@gmail.com>
2024-04-22 01:41:40 -0500

    Creation of todos with serializer

--- todoapp/backend/api/views.py
+++ todoapp/backend/api/views.py
@@ -2,7 +2,7 @@ from rest_framework import generics
 from .serializers import ToDoSerializer
 from todo.models import ToDo
 
-class TodoList(generics.ListAPIView):
+class TodoListCreate(generics.ListCreateAPIView):
     #ListAPIView requieres two mandatory attributes, serializer_class and
     #queryset
     #We specify TodoSerializer wich we have earlier implemented
@@ -11,3 +11,7 @@ class TodoList(generics.ListAPIView):
     def get_queryset(self):
         user = self.request.user
         return ToDo.objects.filter(user=user).order_by('-created')
+    
+    def perform_create(self, serializer):
+        #Serializer holds a django model
+        serializer.save(user=self.request.user)
